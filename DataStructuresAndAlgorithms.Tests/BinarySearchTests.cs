8598230ddae94c91587b7cbb1914525f03df808a
peter-fullstack <peter.fullstack@gmail.com>
2025-08-10 19:18:28 +1000

    Array operations and tests

--- DataStructuresAndAlgorithms.Tests/BinarySearchTests.cs
+++ DataStructuresAndAlgorithms.Tests/BinarySearchTests.cs
@@ -20,5 +20,42 @@ namespace DataStructuresAndAlgorithms.Tests
 
             Assert.Equal(-1, index);
         }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] sortedData = [1, 4, 6, 9, 14, 23];
+            var index = AlgorithmsDataStructureImplementations.BinarySearch(sortedData, 9);
+
+            Assert.Equal(3, index);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] sortedData = [2, 5];
+            var index = AlgorithmsDataStructureImplementations.BinarySearch(sortedData, 5);
+
+            Assert.Equal(1, index);
+        }
+
+
+        [Fact]
+        public void Test5()
+        {
+            int[] sortedData = [-1, 0, 5];
+            var index = AlgorithmsDataStructureImplementations.BinarySearch(sortedData, 5);
+
+            Assert.Equal(2, index);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int[] sortedData = [0, 2, 3, 4, 2, 1, 0];
+            var result = AlgorithmsDataStructureImplementations.ValidMountainArray(sortedData);
+
+            Assert.True(result);
+        }
     }
 }
--- DataStructuresAndAlgorithms/AlgorithmsDataStructureImplementations.cs
+++ DataStructuresAndAlgorithms/AlgorithmsDataStructureImplementations.cs
@@ -4,8 +4,44 @@
     {
         // Assumes the array is sorted - returns the index 
         // at which the search value is found
+        public static int BinarySearch(int[] nums, int target)
+        {
+            var low = 0;
+            var high = nums.Length - 1;
+
+            return binary_search(nums, low, high, target);
+        }
+
+
         public static int binary_search(int[] array, int low, int hi, int searchValue)
         {
+            if(array.Length == 0) return -1;
+
+            if(array.Length == 1)
+            {
+                if (array[0] == searchValue)
+                {
+                    return 0;
+                }
+
+                return -1;
+            }
+
+            if (array.Length == 2)
+            {
+                if (array[0] == searchValue)
+                {
+                    return 0;
+                }
+
+                if (array[1] == searchValue)
+                {
+                    return 1;
+                }
+
+                return -1;
+            }
+
             do
             {
                 var midpoint = low + (hi - low) / 2;
@@ -19,6 +55,10 @@
                 else if (searchValue > midpointValue)
                 {
                     low = midpoint + 1;
+                    if (array[low] == searchValue)
+                    {
+                        return low;
+                    }
                 }
                 else if (searchValue < midpointValue)
                 {
@@ -76,6 +116,77 @@
                     }
                 }
             }
+        }
+
+        /*
+         Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+         Recall that arr is a mountain array if and only if:
+
+         arr.length >= 3
+         There exists some i with 0 < i < arr.length - 1 such that:
+         arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+         arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+         
+         [0, 2, 3, 4, 5, 2, 1, 0] - continuous incline and decline
+
+         [0, 2, 3, 3, 5, 2, 1, 0] - incline is broken by a plateua 3, 3
+
+         */
+
+        public static bool ValidMountainArray(int[] arr)
+        {
+            if(arr.Length < 3) return false;
+
+            // To meet the criteria the array must have an incline - ascending values
+            // followed by a decling descinding values
+
+            // Check for incline
+            var inclineCount = 0;
+            int i = 0;
+            for (i = 0; i < arr.Length - 1;i++)
+            {
+                if(arr[i] < arr[i + 1])
+                {
+                    inclineCount++;
+                }
+            }
+
+            inclineCount++;
+
+            if (inclineCount == arr.Length)
+            {
+                return false;
+            }
+
+            // currently i is the top of the mountain
+            var j = inclineCount;
+            //j++;
+
+            // i is last element in array
+            var declneCount = 0;
+            if (j == arr.Length - 1)
+            {
+                if (arr[j] < arr[inclineCount])
+                {
+                    declneCount++;
+                }
+            }
+            
+            while(j <  arr.Length - 1) 
+            {
+                if (arr[j] > arr[j  + 1])
+                {
+                    declneCount++;
+                }
+
+                j++;
+            }
+
+            declneCount++;
+
+            return declneCount + inclineCount == arr.Length;
         }
     }
 }
